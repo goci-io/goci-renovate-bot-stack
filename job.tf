1c42c3f5618eeb8fc0304a3210c9184ee0101c74
etwillbefine <tim.gatzemeier@gmx.de>
2020-09-09 16:28:09 +0200

    separate job file, create private/public key

---
+++ job.tf
@@ -0,0 +1,97 @@
+resource "kubernetes_cron_job" "renovate" {
+  metadata {
+    name      = var.name
+    namespace = var.k8s_namespace
+  }
+
+  spec {
+    failed_jobs_history_limit     = 3
+    successful_jobs_history_limit = 1
+    starting_deadline_seconds     = 300
+    suspend                       = false
+    schedule                      = var.schedule
+    concurrency_policy            = "Replace"
+
+    job_template {
+      metadata {
+        labels = {
+          app     = "renovatebot"
+          release = var.name
+        }
+      }
+
+      spec {
+        backoff_limit              = 3
+        ttl_seconds_after_finished = 1800
+
+        template {
+          metadata {}
+
+          spec {
+            restart_policy = "OnFailure"
+
+            volume {
+              name = "config"
+
+              config_map {
+                name = kubernetes_config_map.renovate_config.metadata.0.name
+              }
+            }
+
+            volume {
+              name = "private-key"
+
+              secret {
+                default_mode = "0400"
+                secret_name  = kubernetes_secret.private_key.metadata.0.name
+              }
+            }
+
+            container {
+              name  = "renovate"
+              image = "renovate/renovate:${var.renovate_version}"
+
+              security_context {
+                run_as_user                = 1000
+                run_as_non_root            = true
+                allow_privilege_escalation = false
+              }
+
+              resources {
+                requests {
+                  cpu    = "300m"
+                  memory = "128Mi"
+                }
+
+                limits {
+                  cpu    = "300m"
+                  memory = "128Mi"
+                }
+              }
+
+              env_from {
+                secret_ref {
+                  name = kubernetes_secret.renovate_secrets.metadata.0.name
+                }
+              }
+
+              volume_mount {
+                read_only  = true
+                name       = "config"
+                sub_path   = "config.json"
+                mount_path = "/usr/src/app/config.json"
+              }
+
+              volume_mount {
+                read_only  = true
+                name       = "private-key"
+                sub_path   = "decrypt.key"
+                mount_path = "/usr/src/app/decrypt.key"
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
+resource "tls_private_key" "encryption" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "kubernetes_secret" "private_key" {
+  metadata {
+    name      = format("%s-key", var.name)
+    namespace = var.k8s_namespace
+  }
+
+  data = {
+    "decrypt.key" = tls_private_key.encryption.private_key_pem
+  }
+}
+
 resource "kubernetes_secret" "renovate_secrets" {
   metadata {
     name      = var.name
@@ -21,103 +37,5 @@ resource "kubernetes_config_map" "renovate_config" {
       git_endpoint = var.git_endpoint
       repositories = jsonencode(var.repositories)
     })
-  }
-}
-
-resource "kubernetes_cron_job" "renovate" {
-  metadata {
-    name      = var.name
-    namespace = var.k8s_namespace
-  }
-
-  spec {
-    failed_jobs_history_limit     = 3
-    successful_jobs_history_limit = 1
-    starting_deadline_seconds     = 300
-    suspend                       = false
-    schedule                      = var.schedule
-    concurrency_policy            = "Replace"
-
-    job_template {
-      metadata {
-        labels = {
-          app     = "renovatebot"
-          release = var.name
-        }
-      }
-
-      spec {
-        backoff_limit              = 3
-        ttl_seconds_after_finished = 1800
-
-        template {
-          metadata {}
-
-          spec {
-            restart_policy = "OnFailure"
-
-            volume {
-              name = "secrets"
-
-              secret {
-                name = kubernetes_secret.renovate_secrets.metadata.0.name
-              }
-            }
-
-            volume {
-              name = "private-key"
-
-              secret {
-                default_mode = "0400"
-                secret_name  = "${var.name}-key"
-              }
-            }
-
-            container {
-              name  = "renovate"
-              image = "renovate/renovate:${var.renovate_version}"
-
-              security_context {
-                run_as_user                = 1000
-                run_as_non_root            = true
-                allow_privilege_escalation = false
-              }
-
-              resources {
-                requests {
-                  cpu    = "300m"
-                  memory = "128Mi"
-                }
-
-                limits {
-                  cpu    = "300m"
-                  memory = "128Mi"
-                }
-              }
-
-              env_from {
-                secret_ref {
-                  name = kubernetes_config_map.renovate_config.metadata.0.name
-                }
-              }
-
-              volume_mount {
-                read_only  = true
-                name       = "config"
-                sub_path   = "config.json"
-                mount_path = "/usr/src/app/config.json"
-              }
-
-              volume_mount {
-                read_only  = true
-                name       = "private-key"
-                sub_path   = "decrypt.key"
-                mount_path = "/usr/src/app/decrypt.key"
-              }
-            }
-          }
-        }
-      }
-    }
   }
 }
